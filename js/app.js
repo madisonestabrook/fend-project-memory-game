c828311dafe2bb789c1f982f4d071fb4a95f4cca
Madison Estabrook <madison.estabrook@gmail.com>
2018-08-12 14:06:44 -0400

    Update Memory Game

--- js/app.js
+++ js/app.js
@@ -11,6 +11,16 @@
  */
 
 // Shuffle function from http://stackoverflow.com/a/2450976
+// Global Variables 
+
+const deck = document.querySelector(.deck); 
+let toggledCards = []; 
+let moves = 0; 
+let clockOff = true; 
+let time = 0; 
+let clockId;
+let matched = 0
+
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
 
@@ -25,6 +35,160 @@ function shuffle(array) {
     return array;
 }
 
+deck.addEventListener("click", event { 
+    const clickTarget = event.target; 
+    if (clickTarget.classList.contains('card')) { 
+        clickTarget.classList.toggle('open'); 
+        clickTarget.classList.toggle('show'); 
+    }
+    if(toggledCards.length ===2) { 
+        checkforMatch(clickTarget); 
+        addMove();
+        checkScore();
+    }
+});
+
+const deck = document.querySelector('.deck'); 
+
+function shuffleDeck() { 
+    const cardtoShuffle = Array.from(document.querySelectorAll('.deck li')); 
+    const shuffleCards = shuffle(cardsToShuffle); 
+}
+
+
+function addMove() { 
+    moves++
+    const movesText = document.querySelector('.moves'); 
+    moversText.innerHTML = moves; 
+}
+
+function hideStar() { 
+    const starList = document.querySelectorAll('.stars li');
+    for (star of starList) { 
+        if(star.style.display !== 'none') { 
+            star.style.display = 'none'; 
+            break;
+        } 
+    } 
+}
+
+function checkScore { 
+    if(moves === 19 || moves === 22) { 
+        removeStar(); 
+    }
+}
+
+function startClock() { 
+    let clockId = setInterval(() { 
+        time++;  
+    }, 1000); 
+}
+
+function displayTime(){ 
+    const clock = document.querySelector('clock'); 
+    cosnt minutes = Math.floor(time / 60); 
+    cosnt seconds = time % 60; 
+    if (seconds < 10) { 
+        clock.innerHTML = `${minutes}:0${seconds}`; 
+    } 
+    else { 
+        clock.innerHTML = `${minutes}:${seconds}`; 
+    }
+}
+
+function stopClock{
+    clearInterval(clockId); 
+}
+
+function toggleModal(){ 
+    const modal = document.querySelector('.modal_background'); 
+    modal.classList.toggle('hide'); 
+}
+
+function writeModalStats() { 
+    const timeStat = document.querySelector('.modal_time'); 
+    const clockTime = document.querySelector('.clock').innerHTML; 
+    const movesStat = document.querySelector('.modal-moves'); 
+    const starsStat = document.querySelector('.modal_stars'); 
+    const stars = getStars(); 
+
+    timeStat.innerHTML = ` Time = ${clockTime}`; 
+    movesStat.innerHTML = ` Moves = ${moves}`; 
+    starsStat.innerHTML = ` Stars = ${stars}`;
+}
+
+function getStars() { 
+    stars = document.querySelectorAll('.stars li'); 
+    starCount = 0; 
+    for (star of stars) {
+        if(star.style.display !== 'none') { 
+            starCount++; 
+        }
+    }
+    return starCount; 
+}
+
+document.querySelector('.restart').addEventListener('click', resetGame);
+document.querySelector('.modal_replay').addEventListener('click', resetGame);
+
+function resetGame(){ 
+    resetClockAndTime(); 
+    resetMoves();
+    resetStars();
+    shuffleDeck(); 
+}
+
+function resetClockAndTime() { 
+    stopClock(); 
+    clockOff = true; 
+    time = 0; 
+    displayTime()
+}
+
+function resetMoves() { 
+    moves = 0; 
+    document.querySelector('.mover').innerHTML = moves; 
+}
+
+function resetStars() { 
+    stars = 0; 
+    const starList = document.querySelectorAll('.stars li'); 
+    for(star of starList) { 
+        star.style.display = 'inline';
+    }
+}
+
+function checkforMatch() { 
+    if (toggledCards[0].firstElementChild.className === toggledCards[1].firstElementChild.className) { 
+        toggledCards[0].classList.toggle('match'); 
+        toggledCards[1].classList.toggle('match'); 
+        toggledCards = []; 
+        matched++; 
+        )
+    }
+    const TOTAL_PAIRS = 8; 
+    if(matched === TOTAL_PAIRS) { 
+        gameOver(); 
+    }
+}
+
+function gameOver() { 
+    stopClock(); 
+    writeModalStats(); 
+    toggleModal(); 
+}
+
+document.querySelector('.modal_replay').addEventListener('click', replayGame)
+function replayGame() { 
+    resetGame(); 
+    toggleModal(); 
+}
+
+function resetCards(){ 
+    cosnt cards =  document.querySelectorAll('.deck li'); 
+    for(let card of cards) P 
+        card.className = 'card';
+}
 
 /*
  * set up the event listener for a card. If a card is clicked:
