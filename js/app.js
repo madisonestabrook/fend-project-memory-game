b99dde961541719c199cb401c7e01d8978e6e42e
Madison Estabrook <madison.estabrook@gmail.com>
2018-08-16 13:10:28 -0400

    Improved Code's Quality

--- js/app.js
+++ js/app.js
@@ -18,7 +18,7 @@ let openedCards = [];
 const restart = document.querySelector(".modal_cancel"); 
 const playAgainButton = document.querySelector(".modal_replay"); 
 
-// Shuffle Function 
+// Shuffle Function (NOT MINE)
 function shuffle(array) {
     var currentIndex = array.length,
         temporaryValue, randomIndex;
@@ -36,22 +36,23 @@ function shuffle(array) {
 
 restart.addEventListener('click', startGame()); 
 
+// startGame() Function 
 function startGame() { 
     stopModal(); 
     openedCards = []; 
     oneCount = 0; 
     cards = shuffle(cards); 
-    deck.innerHTML = "";
+    deck.innerHTML = ""; // Clears the current deck 
     cards.forEach(function(card) {
         card.classList.remove("show", "open", "match"); 
         deck.appendChild(card); 
-        }); 
+        }); // For each card, remove the classes "show", "open", and "match"; also, add each card to the deck  
     moves = 0; 
     stars.length = 3; 
     stars.forEach(function(star) {
         star.className = ' '; 
         star.classList.add('fas', 'fa-star'); 
-    });
+    }); // For each card, reset the className and add the class names "fas" as well as "fa-star"
     seconds = 0; 
     minutes = 0; 
     min.innerHTML = '00'; 
@@ -60,39 +61,47 @@ function startGame() {
     clearInterval(gameTime); 
 }
 
+// stopModal() Function
 function stopModal() { 
     clearTimeout(results); 
 }
 
+// cardClicks() function 
 function cardClicks(){ 
-    cards.forEach(function(card) {
+    cards.forEach(function(card) { // For all the cards, 
         card.addEventListener('click', flipCard); 
         card.addEventListener('click', matchedEval); 
         card.addEventListener('click', gameOver); 
         card.addEventListener('click', clickToStart, { 
             once: true
-        }); 
+        }); // Add event listeners that call various functions when a card is clicked
     });
 }
 
+// flipCard() Function 
 function flipCard(){ 
     this.classList.toggle('open'); 
     this.classList.toggle('show'); 
-}
+} // For each of the card classes above, toggle (see https://developer.mozilla.org/en-US/docs/Web/API/Element/classList for more info)
 
+
+// matchedEval() Function 
 function matchedEval() { 
-    openedCards.push(this); 
-    if(openedCards.length === 2) { 
-        moveCounter(); 
-    if(openedCards[0].firstElementChild.getAttribute("class") === openedCards[1].firstElementChild.getAttribute("class")){ 
-        weGotAMatch(); 
+    openedCards.push(this); // Add the current card to the list of opened card
+    if(openedCards.length === 2) { // If there are 2 opened cards, 
+        moveCounter();  // Call the moveCounter() function
+    if(openedCards[0].firstElementChild.getAttribute("class") === openedCards[1].firstElementChild.getAttribute("class")){ /* 
+        If the first element (index 0) of openedCard's firstElementChild's class matches the same for the second element (index 1) of opened cards */
+        weGotAMatch(); // Call the weGotAMatch function 
     }
     else if(openedCards[0].firstElementChild.getAttribute("class") !== openedCards[1].firstElementChild.getAttribute("class")) { 
-        weDoNotHaveAMatch(); 
+        // If the opened card's classes do NOT match, 
+        weDoNotHaveAMatch();  // Call the weDoNotHaveAMatch function
     }
     }
 }
 
+// Thank you @drunkenismet for this OOP snippet and again for helping me
 Object.defineProperty(HTMLElement.prototype, "classListChain", {
     get: function () {
       let self = this;
@@ -109,33 +118,38 @@ Object.defineProperty(HTMLElement.prototype, "classListChain", {
     }
   });
 
+//weGotAMatch() Function  
 function weGotAMatch() { 
-    console.log("Your cards match!"); 
-    openedCards[0].classListChain.add("match").classListChain.remove("show", "open"); 
-    openedCards[1].classListChain.add("match").classListChain.remove("show", "open"); 
-    openedCards = []; 
+    console.log("Your cards match!"); // Prints the message in quotes to the console 
+    openedCards[0].classListChain.add("match").classListChain.remove("show", "open"); // Adds the class match plus removes the classes show and open from the first card
+    openedCards[1].classListChain.add("match").classListChain.remove("show", "open");  // Adds the class match plus removes the classes show and open from the second card
+    openedCards = []; // Resets the openedCards variable
 }
 
+// weDoNotHaveAMatch() Function
 function weDoNotHaveAMatch() { 
-    setTimeout(function() {
-            openedCards[0].classListChain.remove("show", "open", "matched"); 
-            openedCards[1].classListChain.remove("show", "open", "matched");
+    setTimeout(function() { // In the number of ms declared on line 135, 
+            openedCards[0].classListChain.remove("show", "open", "matched"); // Removes the 3 listed classes from the first card
+            openedCards[1].classListChain.remove("show", "open", "matched"); // Removes the 3 listed classes from the second card
             openedCards = []; 
     }, 678); 
 }
 
+// moveCounter() Function
 function moveCounter() { 
     moves++; 
-    (moves !== 1) ? (moveCounterCONST.innerHTML = `${moves} moves`) : ((moveCounterCONST.innerHTML = `${moves} move`), (seconds = 0, minutes = 0)); 
-    if(moves > 9 && moves <= 12) { 
-        removeSomeStars(); 
+    (moves !== 1) ? (moveCounterCONST.innerHTML = `${moves} moves`) : ((moveCounterCONST.innerHTML = `${moves} move`), (seconds = 0, minutes = 0));
+     // Ternary statement (condition) : (if-true-then): (if-false-then) 
+    if(moves > 9 && moves <= 12) { // If moves is greater than 9 and less than or equal to 12, 
+        removeSomeStars(); // Call the removeSomeStars() function
     }
-    if(moves > 12 && moves < 15) { 
-        removeAllStars(); 
+    if(moves > 12 && moves < 15) { // If moves is greater than 12 and less than 15, 
+        removeAllStars(); // Call the removeAllStats() function
     }
-    return moves; 
+    return moves; // Return moves
 }
 
+// startTimer() Function 
 function startTimer() { 
     gameTime = setInterval(function() { 
             seconds++; 
@@ -153,29 +167,35 @@ function startTimer() {
             }
             if(minutes > 9) { 
                 min.innerHTML =  `${minutes}`; 
-            }
+            } // The function above displays the time in an easy-to-read format
     }, 1000);
 }
 
+// clickToStart() Function
 function clickToStart() { 
-    oneCount++; 
+    oneCount++;  
     for(i = 0; i < oneCount; i++) { 
         startTimer(); 
     }
 }
 
+// removeSomeStars() Function
 function removeSomeStars() { 
     stars[i].classListChain.remove("fa-star").classListChain.add("fa-star-half");
+    // Adds and removes classes from stars
 }
 
+// removeAllStars() Function 
 function removeAllStars() { 
-    stars[i].classListChain.remove('fas',"fa-star-half").classListChain.add("far", "fa-star")
+    stars[i].classListChain.remove('fas',"fa-star-half").classListChain.add("far", "fa-star"); 
+    // Adds and removes classes from stars 
 }
 
+// gameOver() Function 
 function gameOver() {
-    if(matchedCard.length === 16) { 
-        clearInterval(gameTime); 
-        let finalTime = `${min.innerHTML}:${secs.innerHTML}`; 
+    if(matchedCard.length === 16) {  // If there are 16 matches, 
+        clearInterval(gameTime); // Clear the internal of gameTime
+        let finalTime = `${min.innerHTML}:${secs.innerHTML}`; // Grabs the final time
         results = setTimeout(function() {
             modal.style.display = 'block';
         }, 2000); 
@@ -189,6 +209,7 @@ function gameOver() {
 
 playAgainButton.addEventListener("click", reset); 
 
+// reset() Function
 function reset(){ 
     modal.style.display = "none"; 
     startGame();
